a038cf182d1dfbd0b15d99f53de23f61e0f3bd89
tdeschamps <thomasdscmps@gmail.com>
2017-07-01 19:58:02 +0200

    uses config_variable for github url

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -8,7 +8,7 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}), do: {:ok, Poison.Parser.parse!(body) } 
