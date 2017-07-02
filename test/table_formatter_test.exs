9f9bcf585e48f6238cd95b6cd4e8ac5734d68f83
tdeschamps <thomasdscmps@gmail.com>
2017-07-02 17:14:00 +0200

    fixes specs

--- test/table_formatter_test.exs
+++ test/table_formatter_test.exs
@@ -5,10 +5,10 @@ defmodule TableFormatterTest do
   alias Issues.TableFormatter, as: TF
 
   def simple_test_data do
-    [ [ c1: "r1 c1", c2: "r2 c2", c3: "r3 c3", c4: "r1+++c4" ],
+    [ [ c1: "r1 c1", c2: "r1 c2", c3: "r1 c3", c4: "r1+++c4" ],
       [ c1: "r2 c1", c2: "r2 c2", c3: "r2 c3", c4: "r2 c4" ], 
       [ c1: "r3 c1", c2: "r3 c2", c3: "r3 c3", c4: "r3 c4" ],
-      [ c1: "r4 c1", c2: "r4 c2", c3: "r4 c3", c4: "r4 c4"]
+      [ c1: "r4 c1", c2: "r4++c2", c3: "r4 c3", c4: "r4 c4"]
     ] 
   end
 
@@ -35,12 +35,12 @@ defmodule TableFormatterTest do
   test "Output is correct" do
     result = capture_io fn -> TF.print_table_for_columns(simple_test_data(), headers()) end
     assert result == """
-  c1    | c2     | c4 
+  c1    | c2     | c4     
   ------+--------+--------
   r1 c1 | r1 c2  | r1+++c4
-  r2 c1 | r2 c2  | r2 c4
-  r3 c1 | r3 c2  | r3 c4
-  r4 c1 | r4 c2  | r4 c4
+  r2 c1 | r2 c2  | r2 c4  
+  r3 c1 | r3 c2  | r3 c4  
+  r4 c1 | r4++c2 | r4 c4  
   """
   end
 end
